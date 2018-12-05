58dc30ee4403cbc7d45f2c6531c7e84ef7e93e81
Alberto Irurueta <alberto@irurueta.com>
2018-12-06 00:37:41 +0100

    Test improvements

--- src/test/java/com/irurueta/geometry/estimators/MSACDLTPointCorrespondencePinholeCameraRobustEstimatorTest.java
+++ src/test/java/com/irurueta/geometry/estimators/MSACDLTPointCorrespondencePinholeCameraRobustEstimatorTest.java
@@ -3184,7 +3184,7 @@ public class MSACDLTPointCorrespondencePinholeCameraRobustEstimatorTest
             CameraException, NotAvailableException {
         int numCovariances = 0;
         int numValid = 0;        
-        for (int t = 0; t < TIMES; t++) {
+        for (int t = 0; t < 2 * TIMES; t++) {
             UniformRandomizer randomizer = new UniformRandomizer(new Random());
             double horizontalFocalLength = randomizer.nextDouble(
                     MIN_FOCAL_LENGTH, MAX_FOCAL_LENGTH);
